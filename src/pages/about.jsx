323d8c6abe31c55b60735b7dec462fcec874080e
mallowbugs <mallowbugs@gmail.com>
2022-11-29 14:33:49 +0900

    🏗️ 拡張子・ディレクトリ構成・インポート方法の変更・_appを用いた処理の共通化

---
+++ src/pages/about.jsx
@@ -0,0 +1,18 @@
+import styles from "src/styles/Home.module.css";
+import Head from "next/head";
+import { Main } from "src/components/Main";
+import { Footer } from "src/components/Footer";
+import { Header } from "src/components/Header";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>about page</title>
+      </Head>
+      <Header />
+      <Main page="about"></Main>
+      <Footer />
+    </div>
+  );
+}
