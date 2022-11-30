77ba5a1c507872d73ff7167d08f2b9090627c66e
mallowbugs <mallowbugs@gmail.com>
2022-11-30 15:23:49 +0900

    ⚡️ useCallbackとuseEffectについて学んだ

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,20 +3,21 @@ import Head from "next/head";
 import { Main } from "src/components/Main";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [num, setNum] = useState(1);
-  const handleClick = (e) => {
-    setNum((num) => num + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (num < 10) {
+      setNum((num) => num + 1);
+    }
+  }, [num]);
   useEffect(() => {
     document.body.style.backgroundColor = "lightgreen";
-
     return () => {
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [num]);
 
   return (
     <div className={styles.container}>
