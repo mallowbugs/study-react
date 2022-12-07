d47c9b75f6e10de83eb0071870bd859344c798f1
y.matsunami <mallowbugs@gmail.com>
2022-12-07 13:55:39 +0900

    ✨ useRouterを学習

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,12 +2,12 @@ import "src/styles/globals.css";
 import Head from "next/head";
 import { useCounter } from "src/hooks/useCounter";
 import { useInputArray } from "src/hooks/useInputArray";
-import { useBGLightGreen } from "src/hooks/useBGLightGreen";
+import { useBgColor } from "src/hooks/useBgColor";
 
 function MyApp({ Component, pageProps }) {
   const counter = useCounter();
   const inputArray = useInputArray();
-  useBGLightGreen();
+  useBgColor();
 
   return (
     <>
