2f2541bd4da3fa63e5cf5ee45266cbb972f8be54
y.matsunami <mallowbugs@gmail.com>
2022-12-20 21:11:16 +0900

    🚚 投稿系ページの名前変更

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,18 +1,18 @@
 import styles from "src/styles/Home.module.css";
 import Head from "next/head";
 import { Header } from "src/components/Header";
-import { Posts } from "src/components/Posts";
+import { Posts as PostsCompornent } from "src/components/Posts";
 
-const Home = () => {
+const Posts = () => {
   return (
     <div className={styles.container}>
       <Head>
         <title>index page</title>
       </Head>
       <Header />
-      <Posts />
+      <PostsCompornent />
     </div>
   );
 };
 
-export default Home;
+export default Posts;
