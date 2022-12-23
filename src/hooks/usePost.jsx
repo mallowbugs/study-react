04a0315f579adad2cdf1a5f2bc4e92681792dc8a
y.matsunami <mallowbugs@gmail.com>
2022-12-23 13:43:37 +0900

    🎨 投稿取得まわりのアーキテクチャが変更

--- src/hooks/usePost.jsx
+++ src/hooks/usePost.jsx
@@ -1,13 +1,9 @@
-import { useRouter } from "next/router";
 import { fetcher } from "src/utils/fetcher";
 import useSWR from "swr";
 
-export const usePost = () => {
-  const router = useRouter();
+export const usePost = (id) => {
   const { data, error, isLoading } = useSWR(
-    router.query.id
-      ? `https://jsonplaceholder.typicode.com/posts/${router.query.id}`
-      : null,
+    id ? `https://jsonplaceholder.typicode.com/posts/${id}` : null,
     fetcher
   );
 
