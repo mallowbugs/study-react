56e5d323da1e8c7fa933367fb9c36d88a73f9968
y.matsunami <mallowbugs@gmail.com>
2023-01-07 14:51:53 +0900

    ♻️ SWRのfetcherをapp.jsxへSWRConfigで共通化

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,8 @@
 import "tailwindcss/tailwind.css";
 import Head from "next/head";
 import { Layout } from "src/components/Layout";
+import { SWRConfig } from "swr";
+import { fetcher } from "src/utils/fetcher";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
@@ -8,9 +10,11 @@ const MyApp = ({ Component, pageProps }) => {
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <SWRConfig value={{ fetcher }}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SWRConfig>
     </>
   );
 };
