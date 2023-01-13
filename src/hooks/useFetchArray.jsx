5de6e1e64d24e7391b41347418280ff40118e61e
y.matsunami <mallowbugs@gmail.com>
2023-01-13 12:46:44 +0900

    🎨 APIのURLをconstから読み込むように変更

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -1,3 +1,4 @@
+import { API_URL } from "src/utils/const";
 import useSWRImmutable from "swr/immutable";
 
 const useFetchArray = (url) => {
@@ -10,8 +11,6 @@ const useFetchArray = (url) => {
     isEmpty: data && data.length === 0,
   };
 };
-
-const API_URL = "https://jsonplaceholder.typicode.com";
 
 // posts
 export const usePosts = () => {
